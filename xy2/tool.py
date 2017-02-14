ef596348118010565e552145a7ecd8a654097a4d
abel <fylaotou@gmail.com>
2017-02-14 10:08:25 +0800

    move bxxm position to abel_window

--- xy2/tool.py
+++ xy2/tool.py
@@ -16,16 +16,16 @@ if __name__ == '__main__':
     rect                 = win32gui.GetWindowRect(w._handle)
     x_offset             = rect[0]
     y_offset             = rect[1]
-    x_head_begin = x_offset + 376
-    y_head_begin = y_offset + 232
-    x_head_end = x_head_begin + 260
-    y_head_end = y_head_begin + 54
+    # x_head_begin = x_offset + 376
+    # y_head_begin = y_offset + 232
+    # x_head_end = x_head_begin + 260
+    # y_head_end = y_head_begin + 54
     im_head = ImageGrab.grab(
         bbox=(
-            x_head_begin,
-            y_head_begin,
-            x_head_end,
-            y_head_end
+            x_offset + abel_window.bxxm_pos[0],
+            y_offset + abel_window.bxxm_pos[1],
+            x_offset + abel_window.bxxm_pos[2],
+            y_offset + abel_window.bxxm_pos[3]
         )
     )
     im_head.save(sys.argv[1]+'.png')
